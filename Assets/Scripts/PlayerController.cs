6bea67e472067dafffce48aee783514183e261ea
qkrwjdtn1236 <48826426+qkrwjdtn1236@users.noreply.github.com>
2021-05-14 12:02:15 +0900

    배경 맵 적용 및 플레이어 이동

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float translation;
+    private float rotation;
+
+    public float PlayerSpeed;
+    public float PlayerRotateSpeed;
+    void Update()
+    {
+        translation = Input.GetAxis("Vertical") * PlayerSpeed;
+
+        rotation = Input.GetAxis("Horizontal") * PlayerRotateSpeed;
+
+        transform.Translate(0, 0, translation*Time.deltaTime);
+        transform.Rotate(0, rotation*Time.deltaTime, 0);
+    }
+}
